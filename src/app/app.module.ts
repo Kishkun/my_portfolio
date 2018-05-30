36eb48a52fcd0a53fe9b9d2cf294fd4ed1997479
Kishkun <durata55@gmail.com>
2018-05-30 14:41:20 +0300

    add slider-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { SliderPageComponent } from './slider-page/slider-page.component';
+import {Ng2CarouselamosModule} from 'ng2-carouselamos';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    SliderPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Ng2CarouselamosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
