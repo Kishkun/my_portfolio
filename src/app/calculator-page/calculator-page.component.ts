67dab165020474d8540fd0fbd83dacd15d6d4528
Kishkun <durata55@gmail.com>
2018-06-04 20:43:59 +0300

    add my calculator component

--- src/app/calculator-page/calculator-page.component.ts
+++ src/app/calculator-page/calculator-page.component.ts
@@ -7,20 +7,49 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CalculatorPageComponent implements OnInit {
 
-  numbers: number;
+  number: number;
   displayResult: string;
   operation: string;
-  newOperations: string
   firstNumber: number;
+  result: number;
   secondNumber: number;
+  newOperations: string;
+  commandOperation: string;
 
 
   constructor() {
-
+    this.displayResult = '';
+    this.newOperations = '';
   }
 
-  onNumberButtonClick() {
-    console.log('click number');
+  onNumberButtonClick(e) {
+    this.number = e.target.innerText;
+    this.displayResult += this.number;
+    this.firstNumber = +this.displayResult;
+  }
+
+  onOperationClick(e) {
+    this.operation = e.target.innerText;
+    if (this.operation === '+') {
+      this.displayResult = '';
+      this.secondNumber = +this.number;
+    } else if (this.operation === '=') {
+      this.result = this.firstNumber + this.secondNumber;
+      this.displayResult = String(this.result);
+      console.log(this.result);
+      console.log(this.firstNumber);
+      console.log(this.secondNumber);
+    }
+  }
+
+  onCommandOperation(e) {
+    this.commandOperation = e.target.innerText;
+    if (this.commandOperation === 'AC') {
+      this.displayResult = '';
+      this.firstNumber = 0;
+      this.secondNumber = 0;
+      console.log(this.commandOperation);
+    }
   }
 
   ngOnInit() {
